253dc1743dd5aef60795cc26605f49a5df5848fc
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2023-05-02 22:35:37 +0900

    update chapter04

--- app/controllers/admin/movies_controller.rb
+++ app/controllers/admin/movies_controller.rb
@@ -1,11 +1,16 @@
 class Admin::MoviesController < ApplicationController
     def index
-          @movies = Movie.all
+      @movies = Movie.all
     end
+
     def new
-        @movie = Movie.new
-      end
+      @movie = Movie.new
+    end
     
+    def edit
+      @movie = Movie.find(params[:id])
+    end
+
     def create
       @movie = Movie.new(movie_params)
       if @movie.save
@@ -16,7 +21,18 @@ class Admin::MoviesController < ApplicationController
         render action: :new
       end
     end
-    
+
+    def update
+      @movie = Movie.find(params[:id])
+      if @movie.update(movie_params)
+        flash[:success] = "更新完了"
+        redirect_to("/admin/movies")
+      else
+        flash[:danger] = "更新失敗"
+        render action: :new
+      end
+    end
+
     private
     def movie_params
       params.require(:movie).permit(:name, :year, :description, :image_url, :is_showing)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/movies', to: "movies#index"
   namespace :admin do
-    resources :movies, only: [:index, :new, :create]
+    resources :movies
     get 'movies', to: "movies#index"
   end
 end
