14fdb28efcb6a958b0340c8e5cf7605be7fb5075
Taiki Yoshikawa <yoshikawataiki@gmail.com>
2023-05-03 02:28:50 +0900

    update chapter06

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,24 @@
 class MoviesController < ApplicationController
-  def index
-    @movies = Movie.all
-  end
+	def index		
+		@rgp = params[:is_showing]
+		@keyp = params[:keyword]
+
+		if @rgp == "" && @keyp == ""
+			@movies = Movie.all
+		end
+
+		if @rgp == "1"
+			@range = Movie.where(is_showing: true)
+		elsif @rgp == "0"
+			@range = Movie.where(is_showing: false)
+		else
+			@range = Movie.all
+		end
+
+		if (@keyp == "")
+			@movies = @range
+		else
+			@movies = @range.where( "name LIKE ? OR description LIKE ? ", "%#{@keyp}%", "%#{@keyp}%")
+		end
+	end
 end
